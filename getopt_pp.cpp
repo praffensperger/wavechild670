d785239fd97c643818cfae945fe6c42b20c22b5f
Peter Raffensperger <praffensperger@gmail.com>
2012-07-10 17:48:08 +1200

    run tidy and push for the first time

--- getopt_pp.cpp
+++ getopt_pp.cpp
@@ -1,3 +1,25 @@
+/************************************************************************************
+Wavechild670 v0.1 
+
+getopt_pp.cpp
+
+By Peter Raffensperger 10 July 2012
+
+Reference:
+Toward a Wave Digital Filter Model of the Fairchild 670 Limiter, Raffensperger, P. A., (2012). 
+Proc. of the 15th International Conference on Digital Audio Effects (DAFx-12), 
+York, UK, September 17-21, 2012.
+
+Note:
+Fairchild (R) a registered trademark of Avid Technology, Inc., which is in no way associated or 
+affiliated with the author.
+
+License:
+Wavechild670 is licensed under the GNU GPL v2 license. If you use this
+software in an academic context, we would appreciate it if you referenced the original
+paper.
+************************************************************************************/
+
 /*
 GetOpt_pp:      Yet another C++ version of getopt.
     Copyright (C) 2007, 2008  Daniel Gutson, FuDePAN
